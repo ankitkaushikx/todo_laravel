b4bdca639dcea34cb782ded7050abee119dec7b2
ankitkaushikx <ankitkaushikx@gmail.com>
2024-06-15 01:41:30 +0530

    Todo Command Created

---
+++ app/Console/Commands/OverdueTodos.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class OverdueTodos extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'todos:overdue';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $this->line('todfdkfj');
+    }
+}
